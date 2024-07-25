696efddce67e74c4c7ae035628e7a6e30465a4c9
polar7bear <sons1998@naver.com>
2024-07-25 22:15:56 +0900

    Fix: 테스트컨테이너 설정

--- src/test/java/com/sns/gobong/TestcontainersConfiguration.java
+++ src/test/java/com/sns/gobong/TestcontainersConfiguration.java
@@ -1,18 +1,33 @@
 package com.sns.gobong;
 
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
+@DataJpaTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public abstract class TestcontainersConfiguration {
 
-	@Bean
+	@Container
 	@ServiceConnection
-	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest")
+			.withDatabaseName("test")
+			.withUsername("test")
+			.withPassword("test");
+
+	static {
+		postgres.start();
+		System.setProperty("DB_URL", postgres.getJdbcUrl());
+		System.setProperty("DB_USERNAME", postgres.getUsername());
+		System.setProperty("DB_PASSWORD", postgres.getPassword());
 	}
 
 }
