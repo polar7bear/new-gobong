4ecba7ee080bb2ac88fba5455c328a76e6ba3165
polar7bear <sons1998@naver.com>
2024-07-26 17:54:19 +0900

    Feat: Validator 빈 등록

---
+++ src/main/java/com/sns/gobong/config/ValidationConfig.java
@@ -0,0 +1,14 @@
+package com.sns.gobong.config;
+
+import jakarta.validation.Validator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class ValidationConfig {
+    @Bean
+    public Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
+}
