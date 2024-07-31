0a7e4e2185e5af9e47a343a33ce6e1988624b4fa
polar7bear <sons1998@naver.com>
2024-07-31 19:25:31 +0900

    Feat: CustomUserDetails 작성

---
+++ src/main/java/com/sns/gobong/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.sns.gobong.service;
+
+import com.sns.gobong.config.security.CustomUserDetails;
+import com.sns.gobong.domain.entity.User;
+import com.sns.gobong.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(email + ": DB에 존재하지 않는 유저입니다."));
+        return new CustomUserDetails(user.getId(), user.getEmail(), user.getPw(), new ArrayList<>()); // TODO: 권한 설정은 어떻게 할 지?
+    }
+}
