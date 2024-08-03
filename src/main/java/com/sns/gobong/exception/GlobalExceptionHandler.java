07d65bc15759fdd0435951893de2634e94697881
polar7bear <sons1998@naver.com>
2024-08-03 16:56:11 +0900

    Feat: 전역 예외처리 클래스 추가

---
+++ src/main/java/com/sns/gobong/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.sns.gobong.exception;
+
+import com.sns.gobong.exception.user.UserAlreadyExistsException;
+import com.sns.gobong.util.api.ApiError;
+import com.sns.gobong.util.api.ApiResponse;
+import com.sns.gobong.util.api.ErrorType;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ApiResponse<Void> handleUserAlreadyExistsException(UserAlreadyExistsException e) {
+        ApiError apiError = new ApiError(ErrorType.CONFLICT, "409", e.getMessage(), e);
+        return new ApiResponse<>("이미 존재하는 회원입니다.", apiError);
+    }
+}
