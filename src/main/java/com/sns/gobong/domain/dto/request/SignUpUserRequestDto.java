4bba35c3af7d6bcd6e0e15a391571df38a1d6caf
polar7bear <sons1998@naver.com>
2024-08-03 21:35:46 +0900

    Fix: 비밀번호 정규식 수정

--- src/main/java/com/sns/gobong/domain/dto/request/SignUpUserRequestDto.java
+++ src/main/java/com/sns/gobong/domain/dto/request/SignUpUserRequestDto.java
@@ -1,6 +1,7 @@
 package com.sns.gobong.domain.dto.request;
 
 import com.sns.gobong.domain.entity.User;
+import com.sns.gobong.domain.status.Role;
 import com.sns.gobong.domain.status.VisibilityStatus;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -24,6 +25,8 @@ public class SignUpUserRequestDto {
     private String email;
 
     @NotBlank(message = "필수 입력란입니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$",
+            message = "영소문자와 특수기호가 1개 이상 포함된 최소 8자 이상, 16자 이하의 길이로 입력해주세요.")
     private String pw;
 
     private String img; // TODO: AWS S3
@@ -41,6 +44,7 @@ public class SignUpUserRequestDto {
                 .tel(dto.getTel())
                 .img(dto.getImg())
                 .visibility(dto.getVisibility())
+                .role(Role.USER)
                 .build();
     }
 }
