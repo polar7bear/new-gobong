07b6a5e10bd18ad28e00b1d3834016c8edc1e7e4
polar7bear <sons1998@naver.com>
2024-08-03 16:55:54 +0900

    Feat: 중복회원 예외처리 추가

---
+++ src/main/java/com/sns/gobong/exception/user/UserAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package com.sns.gobong.exception.user;
+
+public class UserAlreadyExistsException extends RuntimeException{
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+
+    public UserAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
