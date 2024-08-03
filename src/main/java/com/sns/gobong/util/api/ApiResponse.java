83dc88bea7ec365e6560b3fc43140646f65b05d4
polar7bear <sons1998@naver.com>
2024-08-03 16:54:23 +0900

    Refactor: 응답 객체 리팩토링

--- src/main/java/com/sns/gobong/util/api/ApiResponse.java
+++ src/main/java/com/sns/gobong/util/api/ApiResponse.java
@@ -1,37 +1,51 @@
 package com.sns.gobong.util.api;
 
+import lombok.Getter;
+
+@Getter
 public class ApiResponse<T> {
 
     private boolean success;
+    private String message;
     private ApiError apiError;
     private T data;
-    private long cost = -1;
+    private String ms = "";
 
     private boolean checkedContext() {
         return ResponseContext.requestAt.get() != null;
     }
 
+    private void calculateCost() {
+        if (checkedContext()) {
+            long cost = System.currentTimeMillis() - ResponseContext.requestAt.get();
+            this.ms = cost + " ms";
+        }
+    }
+
     public ApiResponse(T data) {
         this.data = data;
-        if (checkedContext()) {
-            this.cost = System.currentTimeMillis() - ResponseContext.requestAt.get();
-        }
+        calculateCost();
     }
 
-    public ApiResponse(boolean success, ApiError apiError) {
-        this.success = success;
+    public ApiResponse(String message, T data) {
+        this.success = true;
+        this.message = message;
+        this.data = data;
+        calculateCost();
+    }
+
+    public ApiResponse(String message, ApiError apiError) {
+        this.success = false;
+        this.message = message;
         this.apiError = apiError;
-        if (checkedContext()) {
-            this.cost = System.currentTimeMillis() - ResponseContext.requestAt.get();
-        }
+        calculateCost();
     }
 
-    public ApiResponse(boolean success, ApiError apiError, T data) {
-        this.success = success;
+    public ApiResponse(String message, ApiError apiError, T data) {
+        this.success = false;
+        this.message = message;
         this.apiError = apiError;
         this.data = data;
-        if (checkedContext()) {
-            this.cost = System.currentTimeMillis() - ResponseContext.requestAt.get();
-        }
+        calculateCost();
     }
 }
