175c221aec18896b3c3ec40fcfc090372998bd8a
polar7bear <sons1998@naver.com>
2024-08-03 16:51:50 +0900

    Feat: 스레드 시작시간 설정

--- src/main/java/com/sns/gobong/util/api/ResponseContext.java
+++ src/main/java/com/sns/gobong/util/api/ResponseContext.java
@@ -2,7 +2,11 @@ package com.sns.gobong.util.api;
 
 public class ResponseContext {
 
-    public static ThreadLocal<Long> requestAt = new ThreadLocal<>();
+    public static ThreadLocal<Long> requestAt = ThreadLocal.withInitial(() -> null);
+
+    public static void setRequestAt(long time) {
+        requestAt.set(time);
+    }
 
     public static void clear() {
         requestAt.remove();
