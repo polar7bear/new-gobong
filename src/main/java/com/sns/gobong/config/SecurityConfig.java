5547163a1b54f088c696e01008d82fea4961ea8e
polar7bear <sons1998@naver.com>
2024-08-03 21:57:19 +0900

    Fix: 시큐리티 설정 수정

--- src/main/java/com/sns/gobong/config/SecurityConfig.java
+++ src/main/java/com/sns/gobong/config/SecurityConfig.java
@@ -1,23 +1,37 @@
 package com.sns.gobong.config;
 
+import com.sns.gobong.config.security.jwt.JwtFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
                 .authorizeHttpRequests(auth ->
                         auth
-                                .anyRequest().permitAll()
+                                .requestMatchers("/users/sign-up", "/users/sign-in").permitAll()
+                                .anyRequest().authenticated()
                 );
 
         return http.build();
--- src/main/java/com/sns/gobong/config/security/jwt/JwtFilter.java
+++ src/main/java/com/sns/gobong/config/security/jwt/JwtFilter.java
@@ -7,12 +7,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Slf4j
+@Component
 public class JwtFilter extends OncePerRequestFilter {
 
     private final TokenProvider tokenProvider;
