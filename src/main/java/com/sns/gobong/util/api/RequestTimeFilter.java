547629543abfc4bf3aa2766148e6903ee53346cc
polar7bear <sons1998@naver.com>
2024-08-03 16:53:29 +0900

    Fix: 요청-응답 시간 필터 작성

---
+++ src/main/java/com/sns/gobong/util/api/RequestTimeFilter.java
@@ -0,0 +1,18 @@
+package com.sns.gobong.util.api;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class RequestTimeFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        try {
+            ResponseContext.setRequestAt(System.currentTimeMillis());
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+            ResponseContext.clear();
+        }
+    }
+}
