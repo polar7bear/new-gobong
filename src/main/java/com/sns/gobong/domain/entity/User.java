29ca0f060c696e450d248e65d536182a6a620f9e
polar7bear <sons1998@naver.com>
2024-08-03 21:34:29 +0900

    Fix: 회원 Role 추가

--- src/main/java/com/sns/gobong/domain/entity/User.java
+++ src/main/java/com/sns/gobong/domain/entity/User.java
@@ -1,9 +1,15 @@
 package com.sns.gobong.domain.entity;
 
+import com.sns.gobong.domain.status.Role;
 import com.sns.gobong.domain.status.VisibilityStatus;
 import com.sns.gobong.util.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Entity
 @Table(name = "gobong_user")
@@ -42,5 +48,11 @@ public class User extends BaseTimeEntity {
     @Column(name = "visibility_status")
     private VisibilityStatus visibility;
 
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.USER;
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
 
 }
