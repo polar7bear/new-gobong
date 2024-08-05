52317b603d942e128853bfb6e3ff375c378fa1e7
polar7bear <sons1998@naver.com>
2024-08-05 22:23:02 +0900

    Feat: 메서드 추가

--- src/main/java/com/sns/gobong/repository/RefreshTokenRepository.java
+++ src/main/java/com/sns/gobong/repository/RefreshTokenRepository.java
@@ -3,5 +3,8 @@ package com.sns.gobong.repository;
 import com.sns.gobong.domain.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
 }
