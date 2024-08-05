3e3047372939be25df399eef778bd1047d234fb6
polar7bear <sons1998@naver.com>
2024-08-05 22:24:21 +0900

    Feat: 로그아웃 구현

--- src/main/java/com/sns/gobong/controller/UserController.java
+++ src/main/java/com/sns/gobong/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.sns.gobong.controller;
 
-import com.sns.gobong.domain.dto.request.SignInUserRequestDto;
-import com.sns.gobong.domain.dto.request.SignUpUserRequestDto;
+import com.sns.gobong.domain.dto.request.UserSignInRequestDto;
+import com.sns.gobong.domain.dto.request.UserSignOutRequestDto;
+import com.sns.gobong.domain.dto.request.UserSignUpRequestDto;
 import com.sns.gobong.domain.dto.response.UserSignInResponseDto;
 import com.sns.gobong.domain.dto.response.UserSignUpResponseDto;
+import com.sns.gobong.service.RefreshTokenService;
 import com.sns.gobong.service.UserService;
 import com.sns.gobong.util.api.ApiResponse;
 import jakarta.validation.Valid;
@@ -19,17 +21,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+    private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/sign-up")
-    public ApiResponse<UserSignUpResponseDto> signUp(@Valid @RequestBody SignUpUserRequestDto dto) {
+    public ApiResponse<UserSignUpResponseDto> signUp(@Valid @RequestBody UserSignUpRequestDto dto) {
         UserSignUpResponseDto response = userService.signUp(dto);
         return new ApiResponse<>("회원가입이 성공적으로 완료되었습니다.", response);
     }
 
     @PostMapping("/sign-in")
-    public ApiResponse<UserSignInResponseDto> signIn(@Valid @RequestBody SignInUserRequestDto dto) {
+    public ApiResponse<UserSignInResponseDto> signIn(@Valid @RequestBody UserSignInRequestDto dto) {
         UserSignInResponseDto response = userService.signIn(dto);
 
         return new ApiResponse<>("로그인이 성공적으로 완료되었습니다.", response);
+    }
+
+    @PostMapping("/sign-out")
+    public ApiResponse<Void> signOut(@RequestBody UserSignOutRequestDto dto) {
+        String message = refreshTokenService.deleteRefreshToken(dto);
+        return new ApiResponse<>(message);
     }
 }
--- src/main/java/com/sns/gobong/service/RefreshTokenService.java
+++ src/main/java/com/sns/gobong/service/RefreshTokenService.java
@@ -1,9 +1,13 @@
 package com.sns.gobong.service;
 
+import com.sns.gobong.config.security.jwt.TokenProvider;
+import com.sns.gobong.domain.dto.request.UserSignOutRequestDto;
 import com.sns.gobong.domain.entity.RefreshToken;
 import com.sns.gobong.domain.entity.User;
+import com.sns.gobong.exception.user.InvalidRefreshTokenException;
 import com.sns.gobong.repository.RefreshTokenRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,10 +16,29 @@ import org.springframework.transaction.annotation.Transactional;
 public class RefreshTokenService {
 
     private final RefreshTokenRepository refreshTokenRepository;
+    private final TokenProvider tokenProvider;
 
     @Transactional
     public void saveRefreshToken(User user, String refreshToken) {
         RefreshToken entity = new RefreshToken(refreshToken, user);
         refreshTokenRepository.save(entity);
+    }
+
+    @Transactional
+    public String deleteRefreshToken(UserSignOutRequestDto dto) {
+        String requestRefreshToken = dto.getRefreshToken();
+        boolean result = tokenProvider.validationRefreshToken(requestRefreshToken);
+        RefreshToken refreshToken = refreshTokenRepository.findByRefreshToken(dto.getRefreshToken())
+                .orElseThrow(() -> new InvalidRefreshTokenException("유효하지 않는 리프레시 토큰입니다."));
+
+        String ogRefreshToken = refreshToken.getRefreshToken();
+        if (result && requestRefreshToken.equals(ogRefreshToken)) {
+            refreshTokenRepository.delete(refreshToken);
+            SecurityContextHolder.clearContext();
+
+            return "로그아웃이 성공적으로 완료되었습니다.";
+        } else {
+            throw new InvalidRefreshTokenException("유효하지 않은 리프레시 토큰입니다.");
+        }
     }
 }
