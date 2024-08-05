7146e8b0fbbf4bbcce9761207383b3debdff6fd2
polar7bear <sons1998@naver.com>
2024-08-05 22:24:44 +0900

    Feat: 필터 및 핸들러 추가

--- src/main/java/com/sns/gobong/config/SecurityConfig.java
+++ src/main/java/com/sns/gobong/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.sns.gobong.config;
 
 import com.sns.gobong.config.security.jwt.JwtFilter;
+import com.sns.gobong.config.security.jwt.TokenAccessDeniedHandler;
+import com.sns.gobong.config.security.jwt.TokenAuthenticationEntryPoint;
+import com.sns.gobong.config.security.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,8 +19,13 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
+    private final TokenProvider tokenProvider;
+    private final TokenAccessDeniedHandler tokenAccessDeniedHandler;
+    private final TokenAuthenticationEntryPoint tokenAuthenticationEntryPoint;
     private final JwtFilter jwtFilter;
 
     @Bean
@@ -23,15 +33,21 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
 
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
 
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
-                .authorizeHttpRequests(auth ->
-                        auth
-                                .requestMatchers("/users/sign-up", "/users/sign-in").permitAll()
-                                .anyRequest().authenticated()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/users/sign-up", "/users/sign-in").permitAll()
+                        .requestMatchers("/users/sign-out").hasRole("USER")
+                        .anyRequest().authenticated()
+                )
+
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .accessDeniedHandler(tokenAccessDeniedHandler)
+                        .authenticationEntryPoint(tokenAuthenticationEntryPoint)
                 );
 
         return http.build();
