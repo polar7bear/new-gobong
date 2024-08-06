16b734ec39fab3db376ad85b62e47eea6886ae82
polar7bear <sons1998@naver.com>
2024-08-06 21:37:39 +0900

    Chore: OAuth2.0 Client 의존성 추가

--- src/main/java/com/sns/gobong/config/SecurityConfig.java
+++ src/main/java/com/sns/gobong/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.sns.gobong.config.security.jwt.JwtFilter;
 import com.sns.gobong.config.security.jwt.TokenAccessDeniedHandler;
 import com.sns.gobong.config.security.jwt.TokenAuthenticationEntryPoint;
 import com.sns.gobong.config.security.jwt.TokenProvider;
+import com.sns.gobong.service.user.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +12,9 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -27,6 +26,7 @@ public class SecurityConfig {
     private final TokenAccessDeniedHandler tokenAccessDeniedHandler;
     private final TokenAuthenticationEntryPoint tokenAuthenticationEntryPoint;
     private final JwtFilter jwtFilter;
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -34,21 +34,25 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
 
 
-                .sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                /*.sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))*/
 
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/users/sign-up", "/users/sign-in").permitAll()
+                        .requestMatchers("/users/sign-up", "/users/sign-in", "/oauth/login/**").permitAll()
                         .requestMatchers("/users/sign-out").hasRole("USER")
                         .anyRequest().authenticated()
                 )
 
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                /*.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
 
                 .exceptionHandling(exceptionHandling -> exceptionHandling
                         .accessDeniedHandler(tokenAccessDeniedHandler)
                         .authenticationEntryPoint(tokenAuthenticationEntryPoint)
-                );
+                )*/
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfoEndpointConfig ->
+                                userInfoEndpointConfig.userService(customOAuth2UserService)));
+        //.oauth2Client(Customizer.withDefaults());
 
         return http.build();
     }
