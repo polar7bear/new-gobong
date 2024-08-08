41f1311c485be4cdc14207384cd2803265cc0b7a
polar7bear <sons1998@naver.com>
2024-08-08 18:54:50 +0900

    Rename: dto 필드명 수정

--- src/main/java/com/sns/gobong/domain/dto/response/oauth/UserDto.java
+++ src/main/java/com/sns/gobong/domain/dto/response/oauth/UserDto.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @Builder
 public class UserDto {
-    private String name;
-    private String userName;
+    private String nickname;
+    private String email;
     private String role;
 }
