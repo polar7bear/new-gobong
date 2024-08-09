91210107ce7fddef7b5835e291ccdca973a48d62
polar7bear <sons1998@naver.com>
2024-08-09 16:06:08 +0900

    Feat: CORS 설정

--- src/main/java/com/sns/gobong/config/WebConfig.java
+++ src/main/java/com/sns/gobong/config/WebConfig.java
@@ -4,9 +4,11 @@ import com.sns.gobong.util.api.RequestTimeFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public FilterRegistrationBean<RequestTimeFilter> requestTimeFilter() {
@@ -14,5 +16,12 @@ public class WebConfig {
         registrationBean.setFilter(new RequestTimeFilter());
         registrationBean.addUrlPatterns("/*");
         return registrationBean;
+    }
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
