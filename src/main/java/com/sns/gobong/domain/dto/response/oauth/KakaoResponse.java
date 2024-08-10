85018f89d34f07aee8e6cddb7d802a4dbe7381d1
polar7bear <sons1998@naver.com>
2024-08-10 22:30:11 +0900

    Feat: 카카오 회원 dto 작성

---
+++ src/main/java/com/sns/gobong/domain/dto/response/oauth/KakaoResponse.java
@@ -0,0 +1,45 @@
+package com.sns.gobong.domain.dto.response.oauth;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class KakaoResponse implements OAuth2Response {
+
+    private final static String ID = "id";
+    private final static String NICKNAME = "nickname";
+    private final static String EMAIL = "email";
+    private final static String PROVIDER = "kakao";
+    private final Map<String, Object> attributes;
+
+    public KakaoResponse(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getProvider() {
+        return PROVIDER;
+    }
+
+    @Override
+    public String getProviderId() {
+        return Optional.ofNullable(attributes.get(ID))
+                .map(Object::toString)
+                .orElse("");
+    }
+
+    @Override
+    public String getEmail() {
+        return Optional.ofNullable(attributes.get(EMAIL)) // 이메일 추출 로직 추가
+                .map(Object::toString)
+                .orElse("");
+    }
+
+    @Override
+    public String getName() {
+        return Optional.ofNullable(attributes.get("properties"))
+                .map(props -> ((Map<?, ?>) props).get(NICKNAME))
+                .map(Object::toString)
+                .orElse("기본닉네임");
+    }
+}
+
